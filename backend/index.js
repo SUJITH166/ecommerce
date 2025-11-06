b13c75c7f150373f5ea738a1292f1df8e2bf849f
Sujith S <sujithsree009@gmail.com>
2025-11-06 20:59:51 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ app.use('/images',express.static('upload/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`${process.env.APP_API_URL}/images/${req.file.filename}`
     })
 })
 //schema for creating products
